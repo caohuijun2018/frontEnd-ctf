d891e01086ae00239b8995c8abaa3ad7dd3d542d
小珺 <caohuijun.chj@alibaba-inc.com>
2022-04-01 21:08:43 +0800

    feature:首页菜单栏

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,13 +1,33 @@
-import { Menu } from 'antd';
+import { Button, Menu } from 'antd';
+import { Header } from 'antd/lib/layout/layout';
+import logo from '../images/logo.jpg';
+
+import './index.less';
 
 const HomeHeader = () => {
   return (
-    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-      {/* {new Array(15).fill(null).map((_, index) => {
-        const key = index + 1;
-        return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-      })} */}
-    </Menu>
+    <Header className="home-header">
+      <div className="logo">
+        <img className="img" src={logo} />
+      </div>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        className="header">
+        <Menu.Item key="center">赛事中心</Menu.Item>
+        <Menu.Item key="topicRanklist">题目排行榜</Menu.Item>
+        <Menu.Item key="userRanklist">用户排行榜</Menu.Item>
+        <Menu.Item key="messageBook">留言板</Menu.Item>
+        <Menu.Item key="notice">公告</Menu.Item>
+      </Menu>
+      <Button size="small" type="primary" className="login-button">
+        登陆
+      </Button>
+      <Button size="small" type="primary" className="register-button">
+        注册
+      </Button>
+    </Header>
   );
 };
 
